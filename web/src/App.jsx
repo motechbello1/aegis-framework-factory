1219fdb813f360737a0f5199ba15baa4e491114e
Bello Muhammad Mustapha <86878766+motechbello1@users.noreply.github.com>
2026-08-13 16:12:23 +0100

    fix: route universal library through catalogue page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from './pages/Dashboard'
 import Ingest from './pages/Ingest'
 import ReviewQueue from './pages/ReviewQueue'
 import RunDetail from './pages/RunDetail'
-import Controls from './pages/Controls'
+import Catalogue from './pages/Catalogue'
 import Published from './pages/Published'
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route index element={<Dashboard />} />
         <Route path="ingest" element={<Ingest />} />
         <Route path="review" element={<ReviewQueue />} />
-        <Route path="controls" element={<Controls />} />
+        <Route path="controls" element={<Catalogue />} />
         <Route path="published" element={<Published />} />
         <Route path="runs/:runId" element={<RunDetail />} />
         <Route path="*" element={<Navigate to="/" replace />} />
